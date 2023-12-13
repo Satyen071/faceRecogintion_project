168f7037cfc5f13824d9d2cfe8b9bdbc02934b95
satyender <satyen071@gmail.com>
2023-12-13 11:04:11 +0530

    reformatting

--- Attendance.py
+++ Attendance.py
@@ -17,15 +17,17 @@ for cls in mylist:
 
 print(classNames)
 
-def findEncodings(images):
+
+def find_encodings(images):
     encodeList = []
     for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGRA2BGR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
-encodeListKnown = findEncodings(images)
+
+encodeListKnown = find_encodings(images)
 
 print('Encoding completed')
 
@@ -34,10 +36,10 @@ cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGRA2BGR)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGRA2BGR)
 
     facesInCurrentFrame = face_recognition.face_locations(imgS)
-    encodeCurrentFrame = face_recognition.face_encodings(imgS,facesInCurrentFrame)
+    encodeCurrentFrame = face_recognition.face_encodings(imgS, facesInCurrentFrame)
 
     for encodeFace, faceLocation in zip(encodeCurrentFrame, facesInCurrentFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
@@ -49,9 +51,9 @@ while True:
             name = classNames[matchIndex].upper()
             print(name)
             y1, x2, y2, x1 = faceLocation
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
     cv2.imshow('webcam', img)
     cv2.waitKey(1)
