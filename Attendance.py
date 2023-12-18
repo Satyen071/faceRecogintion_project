c6032dfcd42f924b7ce69d41ea02e69151c0204b
satyender <satyen071@gmail.com>
2023-12-18 16:45:27 +0530

    changes needed in mark attendance function

--- Attendance.py
+++ Attendance.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'ImagesAttendence'
 images = []
@@ -27,24 +28,45 @@ def find_encodings(images):
     return encodeList
 
 
+def mark_attendance(name):
+    nameList = []
+    with open('attendance.csv', 'r+') as f:
+        my_data_list = f.readline()
+        print(my_data_list.split(',')[0])
+
+        # for line in my_data_list:
+        #     entry = line.split(',')
+        #     nameList.append(entry[0])
+        nameList.append(my_data_list.split(',')[0])
+
+
+    with open('attendance.csv', 'a') as w:        # print(nameList)
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%HH:%MM:%SS')
+            w.write(f'\n{name},{dtString}')
+
+
 encodeListKnown = find_encodings(images)
 
 print('Encoding completed')
 
 cap = cv2.VideoCapture(0)
+cap.set(10, 100)
 
 while True:
-    success, img = cap.read()
+    ret, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGRA2BGR)
 
     facesInCurrentFrame = face_recognition.face_locations(imgS)
     encodeCurrentFrame = face_recognition.face_encodings(imgS, facesInCurrentFrame)
+    # print('image has been read')
 
     for encodeFace, faceLocation in zip(encodeCurrentFrame, facesInCurrentFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDistance = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDistance)
+        # print(faceDistance)
         matchIndex = np.argmin(faceDistance)
 
         if matches[matchIndex]:
@@ -54,6 +76,10 @@ while True:
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1,
+                        (255, 255, 255), 2)
+            mark_attendance(name)
+
+
     cv2.imshow('webcam', img)
     cv2.waitKey(1)
